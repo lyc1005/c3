77292fcae98c4b68fbf8a7b70ece67e4cba8c779
root <liuyichen961005@icloud.com>
2021-01-15 06:38:08 +0000

    update

--- bert/run_classifier.py
+++ bert/run_classifier.py
@@ -269,18 +269,11 @@ def _truncate_seq_pair(tokens_a, tokens_b, max_length):
 def _truncate_seq_tuple(tokens_a, tokens_b, tokens_c, max_length):
     """Truncates a sequence tuple in place to the maximum length."""
 
-    # This is a simple heuristic which will always truncate the longer sequence
-    # one token at a time. This makes more sense than truncating an equal percent
-    # of tokens from each, since if one sequence is very short then each token
-    # that's truncated likely contains more information than a longer sequence.
+    # always to truncate tokens_a (the passage part in our case) 
     while True:
         total_length = len(tokens_a) + len(tokens_b) + len(tokens_c)
         if total_length <= max_length:
             break
-        if len(tokens_a) >= len(tokens_b) and len(tokens_a) >= len(tokens_c):
-            tokens_a.pop()
-        elif len(tokens_b) >= len(tokens_a) and len(tokens_b) >= len(tokens_c):
-            tokens_b.pop()
         else:
             tokens_c.pop()            
 
